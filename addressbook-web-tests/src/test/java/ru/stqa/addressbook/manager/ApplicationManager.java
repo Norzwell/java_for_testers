8ca0a6f592f8587aa050aa4dcb547ed61360ebed
Norzwell <alucard56632@gmail.com>
2024-05-12 20:01:48 +0300

    Библиотека Hibernate

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/manager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/manager/ApplicationManager.java
@@ -14,6 +14,8 @@ public class ApplicationManager {
     private GroupHelper groups;
     private AddressHelper address;
     private jdbcHelper jdbc;
+    private HibernateHelper hbm;
+
 
     private Properties properties;
 
@@ -61,6 +63,13 @@ public class ApplicationManager {
             jdbc = new jdbcHelper(this);
         }
         return jdbc;
+    }
+
+    public HibernateHelper hbm() {
+        if (hbm == null) {
+            hbm = new HibernateHelper(this);
+        }
+        return hbm;
     }
 
     public boolean isElementPresent(By locator) {
